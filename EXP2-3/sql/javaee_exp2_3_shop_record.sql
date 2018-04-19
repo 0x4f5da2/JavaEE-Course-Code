65e913b6b3ffb893081fa1cd5e99125f0a94c5a3
Zhicheng Chen <chen1319276472@foxmail.com>
2018-04-19 22:22:26 +0800

    update_sql

---
+++ EXP2-3/sql/javaee_exp2_3_shop_record.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: javaee_exp2_3
+-- ------------------------------------------------------
+-- Server version	5.7.20-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `shop_record`
+--
+
+DROP TABLE IF EXISTS `shop_record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shop_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) DEFAULT NULL,
+  `productid` int(11) DEFAULT NULL,
+  `number` int(11) DEFAULT NULL,
+  `shopdate` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shop_record`
+--
+
+LOCK TABLES `shop_record` WRITE;
+/*!40000 ALTER TABLE `shop_record` DISABLE KEYS */;
+INSERT INTO `shop_record` VALUES (1,1,1,1,'2018-04-19'),(2,1,2,3,'2018-04-19'),(3,1,2,1,'2018-04-17'),(4,2,2,1,'2018-04-19'),(5,1,888,1,'2018-04-19'),(6,1,6,1,'2018-04-19'),(7,1,5,1,'2018-04-19'),(8,2,2,1,'2018-04-19'),(9,1,1,1,'2018-04-19'),(10,1,1,1,'2018-04-19'),(11,1,1,1,'2018-04-19'),(12,1,1,1,'2018-04-19'),(13,1,1,1,'2018-04-19'),(14,1,1,1,'2018-04-19');
+/*!40000 ALTER TABLE `shop_record` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-19 22:20:59
